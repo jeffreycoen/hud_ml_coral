c07920207a79109ea5e95a346aa574aab8ebbc21
jeffreycoen <jeff.coen@gmail.com>
2019-05-05 13:20:32 -0400

    update

--- examples-camera-master/gstreamer/detect.py
+++ examples-camera-master/gstreamer/detect.py
@@ -57,7 +57,7 @@ def generate_svg(dwg, objs, labels, text_lines):
         shadow_text(dwg, x, y - 5, label)
         dwg.add(dwg.rect(insert=(x,y), size=(w, h),
                         # fill='red', fill_opacity=0.3, stroke='white'))
-			fill='red', fill_opacity=0.3, stroke='red', stoke_width=10))
+			fill='red', fill_opacity=0.3, stroke='red'))
 
 def main():
     default_model_dir = '../all_models'
