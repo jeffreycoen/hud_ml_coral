a2b35cc060e6b998f8357ba58f2576b75e923d4a
jeffreycoen <jeff.coen@gmail.com>
2019-05-05 13:08:24 -0400

    stroke width

--- examples-camera-master/gstreamer/detect.py
+++ examples-camera-master/gstreamer/detect.py
@@ -57,7 +57,7 @@ def generate_svg(dwg, objs, labels, text_lines):
         shadow_text(dwg, x, y - 5, label)
         dwg.add(dwg.rect(insert=(x,y), size=(w, h),
                         # fill='red', fill_opacity=0.3, stroke='white'))
-			fill='red', fill_opacity=1.0, stroke='white'))
+			fill='red', fill_opacity=0.3, stroke='red', stoke_width=10))
 
 def main():
     default_model_dir = '../all_models'
@@ -71,7 +71,7 @@ def main():
     parser.add_argument('--top_k', type=int, default=3,
                         help='number of classes with highest score to display')
     # original default threshhold 0.1
-    parser.add_argument('--threshold', type=float, default=0.5,
+    parser.add_argument('--threshold', type=float, default=0.7,
                         help='class score threshold')
     args = parser.parse_args()
 
